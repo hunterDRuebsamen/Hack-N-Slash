1ccf081c7078910e4b2ee9e9d9b6f5f9df77cab1
Hunter <huntermosnter8@gmail.com>
2021-09-18 17:23:58 -0700

    Improved Character Controller
    new character controller include acceleration, deceleration, and collision functionality with the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] 
-    int movementSpeed = 1;
+    [SerializeField, Tooltip("Max horizontal speed, in units per second, that the character moves.")]
+    float horizontalSpeed = 9;
 
-    private float horizontal;
-    private float vertical;
-
-    void Update()
-    {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+    [SerializeField, Tooltip("Max vertical speed, in units per second, that the character moves.")]
+    float verticalSpeed = 9;
+ 
+ 
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float acceleration = 75;
+ 
+    [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
+    float deceleration = 70;
+ 
+    private CapsuleCollider2D capsuleCollider;
+ 
+    private Vector2 velocity;
+ 
+    private void Awake()
+    {      
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
-    void FixedUpdate()
+ 
+    private void Update()
     {
-        Vector3 movement = new Vector3(horizontal * movementSpeed, vertical * movementSpeed, 0.0f);
-        transform.position = transform.position + movement * Time.deltaTime;
-    }
+        // Use GetAxisRaw to ensure our input is either 0, 1 or -1.
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+ 
+ 
+        if (horizontalInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, horizontalSpeed * horizontalInput, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
 
+        if (verticalInput != 0) 
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, verticalSpeed * verticalInput, acceleration * Time.deltaTime);
+        }
+        else 
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, 0, deceleration * Time.deltaTime);
+        }
+ 
+        transform.Translate(velocity * Time.deltaTime);
+ 
+        // Retrieve all colliders we have intersected after velocity has been applied.
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, capsuleCollider.size, 0);
+ 
+        foreach (Collider2D hit in hits)
+        {
+            // Ignore our own collider.
+            if (hit.tag == "Player")
+                continue;
+ 
+            ColliderDistance2D colliderDistance = hit.Distance(capsuleCollider);
+ 
+            // Ensure that we are still overlapping this collider.
+            // The overlap may no longer exist due to another intersected collider
+            // pushing us out of this one.
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+            }
+        }
+    }
 }
