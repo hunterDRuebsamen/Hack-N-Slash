0525d00a67242ac3d998335a8c4a452e10ff914a
gururise <gururise@gmail.com>
2021-10-05 18:09:46 -0700

    Enemy Attack Changes

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,33 +1,50 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
     [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
     float speed = 9;
-
+    [SerializeField, Tooltip("Attack damage")]
+    public float damage = 1f;
+    [SerializeField, Tooltip("Attack distance")]
+    private const float attackDist = 2.5f;
+    [SerializeField, Tooltip("Attack cooldown in seconds")]
+    float cooldown = 2f;
     [SerializeField, Tooltip("target of the enemy pathing")]
     GameObject target;
     private CapsuleCollider2D capsuleCollider;
-    private Vector2 velocity;
+    private GameObject weaponGameObject;
+    private SpriteRenderer enemyBodySprite;
+    private Animation attackAnimation;
+
+    private const float weaponXpos = 0.68f;
+
+    private bool isAttacking; 
+
+    public static event Action<float> onPlayerDamaged;
+    public static event Action onAttack;
 
     void Awake()
     {
-        capsuleCollider = GetComponent<CapsuleCollider2D>();    
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        enemyBodySprite = GetComponent<SpriteRenderer>();
+        attackAnimation = transform.GetChild(0).GetComponent<Animation>();
+        weaponGameObject = transform.GetChild(0).gameObject;  // grab first child gameobject
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed*Time.deltaTime);
+        Move();
         // Retrieve all colliders we have intersected after velocity has been applied.
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, capsuleCollider.size, 0);
  
         foreach (Collider2D hit in hits)
         {
             // Ignore our own collider.
-            if (hit.tag == "Enemy" || hit.tag == "Weapon")
+            if (hit.tag == "Enemy" || hit.tag == "EnemyWeapon")
                 continue;
  
             ColliderDistance2D colliderDistance = hit.Distance(capsuleCollider);
@@ -42,6 +59,57 @@ public class EnemyBehavior : MonoBehaviour
         }
     }
 
-    
+    void Move() 
+    {
+        // check if player is to the right or left of enemy, flip enemy gameobjects based on player position
+        if (target.transform.position.x > transform.position.x)
+        {
+            enemyBodySprite.flipX = true;
+            weaponGameObject.transform.localPosition = new Vector3(weaponXpos,weaponGameObject.transform.localPosition.y,0);
+        }
+        else
+        {
+            enemyBodySprite.flipX = true;
+            weaponGameObject.transform.localPosition = new Vector3(-weaponXpos,weaponGameObject.transform.localPosition.y,0);
+        }
 
+        float distToPlayer = Math.Abs(target.transform.position.x - transform.position.x);
+        if (distToPlayer <= attackDist)
+        {
+            //Debug.Log("attack");
+            // attack the player
+            if (!isAttacking) {
+                attackAnimation.Play("EnemySword");
+                onAttack?.Invoke();
+            }
+            StartCoroutine(AttackCoolDown(cooldown));
+        } else {
+            // move toward player
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed*Time.deltaTime);
+        }
+    }
+
+    IEnumerator AttackCoolDown(float time) { 
+        if (!isAttacking) 
+        {
+            isAttacking = true;
+            yield return new WaitForSeconds(time);     // wait for 3 seconds until you can dodge
+            isAttacking = false;
+        }
+    }
+
+    // accessor function for the isAttacking bool
+    public bool isEnemyAttacking() 
+    {
+        return isAttacking;
+    }
+
+    public float getWeaponDamage() {
+        return damage;
+    }
+
+    public void damagePlayerEvent() {
+        // send the onPlayerDamaged event.  This public function can be called from the EnemyBaseWeapon script
+        onPlayerDamaged?.Invoke(getWeaponDamage());
+    }
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource effectSource1;
+
+    [SerializeField]
+    AudioClip hitImpactClip;
+
+    // Start is called before the first frame updat
+
+    // Register events to listen for
+    private void OnEnable() { // Watches for when the enemy gets hit
+        EnemyBehavior.onPlayerDamaged += onPlayerHit;
+    } 
+    private void onDisable() {
+        EnemyBehavior.onPlayerDamaged -= onPlayerHit;
+    } 
+
+    private void onPlayerHit(float dmg)
+    {
+        Debug.Log("play auido");
+        effectSource1.PlayOneShot(hitImpactClip);
+    }
+}
