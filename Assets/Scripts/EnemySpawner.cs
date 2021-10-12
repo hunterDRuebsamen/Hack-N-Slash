72b3eaa0f49ca813e064ef025ae112bbef33f11d
Brian Tran <bktran0@gmail.com>
2021-10-12 15:43:41 -0700

    Prototype of the EnemySpawner is created
    Items achieved: limit num of enemies spawned, time inbetween each spawn, spawns relatively off screen
    
    Currently need to have it spawn directly away from the player instead of the spawner object

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip("The enemies that will be spawned")]
     public GameObject enemyPrefab1;
-    // Start is called before the first frame update
+    [SerializeField, Tooltip("Takes the x position of the player or GameManager as a whole to determine where the enemy is spawned")]
+    public float fromPlayer; //= transform.position.x
+   [SerializeField, Tooltip("The maximum number of enemies that will appear")]
+    public float enemyLimit ;
+    [ Tooltip("The current amount of enemies on screen")]
+    private float enemyCount = 0;
+    [SerializeField, Tooltip("How much time should occur between spawns")]
+    public float spawnTime;
+    [SerializeField, Tooltip("Store the time taken inbetween spawns and tells the system when to spawn an enemy")]
+    private float btwSpawns;
+
+    private void Update()
+    {
+        if (btwSpawns <= 0 && enemyCount < enemyLimit)
+        {
+            Instantiate(enemyPrefab1, new Vector3(fromPlayer + 10, 0, 0) , Quaternion.identity);
+            btwSpawns = spawnTime;
+            enemyCount += 1;
+        }
+        else
+        {
+            btwSpawns -= Time.deltaTime;
+        }
+    }
     void Start()
     {
         // Spawn one enemy when the game starts at position -5,0,0
-        Instantiate(enemyPrefab1, new Vector3(-5,0,0), Quaternion.identity);
+        //Instantiate(enemyPrefab1, new Vector3(-5,0,0), Quaternion.identity);
     }
 }
