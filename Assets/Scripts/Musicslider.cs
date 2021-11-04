4be773f97bb2b2d59dfd04c133296927a64fac84
Gene R <gururise@gmail.com>
2021-11-04 09:07:54 -0700

    Add background music to MainMenu

--- Assets/Scripts/Musicslider.cs
+++ Assets/Scripts/Musicslider.cs
@@ -6,24 +6,29 @@ using UnityEngine.UI;
 public class Musicslider : MonoBehaviour
 {
     [SerializeField]
-    private float defaultMusicVolume = 0.5f;
+    private float defaultMusicVolume = 0.2f;
     private Slider volumeSlider;
+    private AudioSource backgroundMusic;
  
     void Start()
     {
         // Get the slider component
-        volumeSlider = GetComponent<Slider>();
+        volumeSlider = GameObject.Find("Slider").GetComponent<Slider>();
+        // Get the background music audio source
+        backgroundMusic = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
 
         // if the key doesn't exist, create it
         if(!GlobalVariables.HasKey("musicVolume"))
         {
             GlobalVariables.Set("musicVolume", defaultMusicVolume);
             volumeSlider.value = defaultMusicVolume;
+            backgroundMusic.volume = defaultMusicVolume;
         }
         else
         {
             // Grab the music Volume from the GlobalConfiguration
             volumeSlider.value = GlobalVariables.Get<float>("musicVolume");
+            backgroundMusic.volume = volumeSlider.value;
         }
 
         // Add a listener to this slider.onValueChanged to invoke ChangeVolume() when the value changes
@@ -32,5 +37,6 @@ public class Musicslider : MonoBehaviour
     public void ChangeVolume()
     {
         GlobalVariables.Set("musicVolume", volumeSlider.value);
+        backgroundMusic.volume = volumeSlider.value;
     }
 }
