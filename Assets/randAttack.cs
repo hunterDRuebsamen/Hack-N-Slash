384f494f9383006627c977dacc3e1f1bd2e7150b
Carla Zuccarini <carlazuccarini1@gmail.com>
2021-11-16 15:51:45 -0800

    Added Boss Animations
    Disabled all enemies and added boss right away for testing purposes. Boss now randomly does a sword swing, stab or uppercut when player is in range. However, the uppercut animation doesn't work.

---
+++ Assets/randAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randAttack : StateMachineBehaviour
+{
+    private int rand;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rand = Random.Range(0, 6);
+        
+        if (rand == 0) {
+            animator.SetTrigger("attack");
+        }
+        else if (rand == 1 || rand == 2 || rand == 3) {
+            animator.SetTrigger("stab");
+        }
+        else if (rand == 4 || rand == 5) {
+            animator.SetTrigger("uppercut");
+        } 
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
