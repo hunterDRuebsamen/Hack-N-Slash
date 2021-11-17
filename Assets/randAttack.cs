4d8790a5d725d580ace4e40800e8f1da633f0961
Carla Zuccarini <carlazuccarini1@gmail.com>
2021-11-17 15:25:39 -0800

    Boss Combos Fixed
    Boss can now perform 3 different combos against the player when in range.

--- Assets/randAttack.cs
+++ Assets/randAttack.cs
@@ -6,36 +6,25 @@ public class randAttack : StateMachineBehaviour
 {
     private int rand;
 
+    EnemyBehavior eb;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        eb = animator.GetComponent<EnemyBehavior>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        rand = Random.Range(0, 7);
         
-        if (rand == 0 || rand == 1) {
-            animator.SetTrigger("stab");
-        }
-        else if (rand == 2 || rand == 3) {
-            animator.SetTrigger("uppercut");
-        }
-        else if (rand == 4 || rand == 5) {
-            animator.SetTrigger("attack");
-        }
-        else if (rand == 7) {
-            animator.SetTrigger("Idle");
-        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
