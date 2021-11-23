f85368bacaed95b2fd46c9822aa9dc816e0ae2f2
Hunter <huntermonster8@gmail.com>
2021-11-22 20:40:47 -0800

    declare enemybehaviorbase class as abstract

--- Assets/Scripts/EnemyScripts/EnemyBehaviorBase.cs
+++ Assets/Scripts/EnemyScripts/EnemyBehaviorBase.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class EnemyBehaviorBase : MonoBehaviour
+public abstract class EnemyBehaviorBase : MonoBehaviour
 {
     protected virtual void Move()
     {}
