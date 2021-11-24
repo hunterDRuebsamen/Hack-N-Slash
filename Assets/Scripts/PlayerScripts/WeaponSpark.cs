5e4a67953469eb6be925419e66b8808ee088ac1e
Brian Tran <bktran0@gmail.com>
2021-11-23 19:43:24 -0800

    Made a blood splatter occur when hitting an enemy
    The player version of this does not currently work

--- Assets/Scripts/PlayerScripts/WeaponSpark.cs
+++ Assets/Scripts/PlayerScripts/WeaponSpark.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,11 @@ using UnityEngine;
 public class WeaponSpark : MonoBehaviour
 {
     [SerializeField] ParticleSystem weaponSparks = null;
+    [SerializeField] ParticleSystem bloodSpark = null;
     [SerializeField] float timeBetweensparks = .5f;
+    [SerializeField] float timeBetweenSplats = 1f;
     [SerializeField] GameObject player = null;
-
+    
     private void Awake()
     {
         weaponSparks.Stop();
@@ -16,12 +19,16 @@ public class WeaponSpark : MonoBehaviour
     private void OnEnable()
     {
         EnemyBehavior.parriedEvent += spark;
+        WeaponBase.onEnemyDamaged += enemySplat;
+        PlayerHealth.onPlayerHealthChanged += playerSplat;
 
     }
 
     private void OnDisable()
     {
         EnemyBehavior.parriedEvent += spark;
+        WeaponBase.onEnemyDamaged -= enemySplat;
+        PlayerHealth.onPlayerHealthChanged -= playerSplat;
     }
 
         
@@ -30,6 +37,36 @@ public class WeaponSpark : MonoBehaviour
         StartCoroutine( sparkEffect(enemy));
     }
 
+    void enemySplat(float test, GameObject enemy)
+    {
+        Debug.Log("Splat should be triggered");
+        StartCoroutine(bloodSplat(enemy));
+    }
+    void playerSplat(int test)
+    {
+        Debug.Log("Splat should be triggered");
+        StartCoroutine(bloodSplat());
+    }
+
+    IEnumerator bloodSplat(GameObject enemy)
+    {
+        weaponSparks.transform.localPosition = enemy.transform.localPosition;
+        bloodSpark.Play();
+        yield return new WaitForSeconds(timeBetweensparks);
+        bloodSpark.Stop();
+    }
+    IEnumerator bloodSplat()
+    {
+        weaponSparks.transform.localPosition = player.transform.localPosition;
+        Quaternion rotate_particle = weaponSparks.transform.localRotation;
+        //rotate_particle.y *= -1;
+        //weaponSparks.transform.localRotation = rotate_particle;
+        bloodSpark.Play();
+        yield return new WaitForSeconds(timeBetweenSplats);
+        //rotate_particle.y *= -1;
+        //weaponSparks.transform.localRotation = rotate_particle;
+        bloodSpark.Stop();
+    }
     IEnumerator sparkEffect(GameObject enemy)
     {
         weaponSparks.transform.localPosition = enemy.transform.localPosition;
